6e4c5a5c9818eb9d505336411acafb5ed2cbcb59
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-25 21:22:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,10 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  
+  planeteer_calls.collect do |x|
+    x.uppercase + "!"
+  end  
+  planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
